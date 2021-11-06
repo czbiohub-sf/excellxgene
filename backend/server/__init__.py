4f346e3bfec4876d536a50bbf5667831f5ae547d
atarashansky <alexander.tarashansky@atarashansky-mba.local>
2021-11-06 12:45:49 -0700

    1.3.0

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -2,7 +2,7 @@ import logging
 import sys
 from backend.common.utils.utils import import_plugins
 
-__version__ = "1.2.19"
+__version__ = "1.3.0"
 display_version = "cellxgene v" + __version__
 
 try:
