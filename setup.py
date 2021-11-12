e744262dd9bc980a99e1e43761a1c0b6c8756c2b
atarashansky <alexander.tarashansky@atarashansky-mba.local>
2021-11-12 15:32:49 -0800

    fixed color issue

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("backend/server/requirements-prepare.txt") as fh:
 
 setup(
     name="excellxgene",
-    version="1.3.4",
+    version="1.3.5",
     packages=find_packages(),
     url="https://github.com/czbiohub/cellxgene",
     license="MIT",
