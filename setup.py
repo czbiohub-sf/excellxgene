9a209e17eab512fa27d352b1f788de773d06a47e
Alexander Tarashansky <atarashansky>
2022-04-13 10:06:44 -0700

    removed Ray for desktop mode

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("backend/server/requirements.txt") as fh:
 
 setup(
     name="excellxgene",
-    version="2.7.1",
+    version="2.7.2",
     packages=find_packages(),
     url="https://github.com/czbiohub/excellxgene",
     license="MIT",
