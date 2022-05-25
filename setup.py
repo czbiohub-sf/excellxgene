9c4e27e5aba65d9e2c21999694c92f41cc28e26d
Alexander Tarashansky <atarashansky>
2022-05-25 10:58:19 -0700

    version update

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("backend/server/requirements.txt") as fh:
 
 setup(
     name="excellxgene",
-    version="2.8.6",
+    version="2.8.7",
     packages=find_packages(),
     url="https://github.com/czbiohub/excellxgene",
     license="MIT",
